f35a0ca4ad5f020d01415f9673b19b3959e7729d
subsingh22 <subodh.k2@gmail.com>
2026-09-13 18:40:08 +0200

    Add files via upload

---
+++ pythonTest3.py
@@ -0,0 +1,6 @@
+score1 = int(input("Enter highest score of class: "))
+scores2 = int(input("Enter lowest score of class: "))
+average = (score1+scores2//2)
+print("Average of class:",average)
+students = ["sara","mary","mark"]
+print(students)
